13ec57e78db3135ac585c5d70d116c94c610580d
Frank-Proulx <frankproulx7@gmail.com>
2021-12-10 15:32:55 -0800

    update script file to account for empty entry

--- anagrams_script.rb
+++ anagrams_script.rb
@@ -3,9 +3,16 @@
 require('./lib/anagrams')
 
 puts "To check for anagrams or antigrams, input your first word or phrase"
-string1 = gets.chomp
-puts "Please enter your second word or phrase"
-string2 = gets.chomp
+string1 = ''
+string2 = ''
+while string1.length < 1
+  puts "Please enter a word or phrase"
+  string1 = gets.chomp
+end
+while string2.length < 1
+  puts "Please enter a second word or phrase"
+  string2 = gets.chomp
+end
 if (string1.split('').any?{|x| x.match(/\d/)}) || (string2.split('').any?{|x| x.match(/\d/)})
   puts "Your entry contains numbers, and words do not, so your typo has been graciously removed for you"
 end
