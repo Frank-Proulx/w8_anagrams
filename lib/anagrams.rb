31420d02dc4bbbeb99437cc30f7e562d05cac097
Frank-Proulx <frankproulx7@gmail.com>
2021-12-10 10:47:45 -0800

    add logic for testing strings as well having passed test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,16 +1,17 @@
 class Anagrams
   def initialize(word1, word2)
-    @word1 = word1.downcase
-    @word2 = word2.downcase
+    @word1 = word1.downcase.split('').delete_if{|x| x.match(/[^a-z]/)}
+    @word2 = word2.downcase.split('').delete_if{|x| x.match(/[^a-z]/)}
   end
 
   def checker
+    
     vowel_array = ['a', 'e', 'i', 'o', 'u', 'y']
-    if (!@word1.split('').any? { |letter| vowel_array.include?(letter) }) || (!@word2.split('').any? { |letter| vowel_array.include?(letter) })
+    if (!@word1.any? { |letter| vowel_array.include?(letter) }) || (!@word2.any? { |letter| vowel_array.include?(letter) })
       "Sorry, but you must enter actual words."
-    elsif !@word1.split('').any? { |letter| @word2.split('').include?(letter) }
+    elsif !@word1.any? { |letter| @word2.include?(letter) }
       "These words don't share any letters and as such are antigrams."
-    elsif @word1.split('').sort == @word2.split('').sort
+    elsif @word1.sort == @word2.sort
       "Congradulations, these are anagrams!"
     end
   end
