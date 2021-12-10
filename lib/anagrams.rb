e7cfcb2997062cfcf356daa7b6e0c3a3edfffab4
Frank-Proulx <frankproulx7@gmail.com>
2021-12-10 09:42:20 -0800

    add logic for filtering out words with no vowels having passed test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,11 +1,14 @@
 class Anagrams
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+    @word1 = word1.downcase
+    @word2 = word2.downcase
   end
 
   def checker
-    if @word1.downcase.split('').sort == @word2.downcase.split('').sort
+    vowel_array = ['a', 'e', 'i', 'o', 'u', 'y']
+    if (!@word1.split('').any? { |letter| vowel_array.include?(letter) }) || (!@word2.split('').any? { |letter| vowel_array.include?(letter) })
+      "Sorry, but you must enter actual words."
+    elsif @word1.split('').sort == @word2.split('').sort
       "These words are anagrams."
     end
   end
