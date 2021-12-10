3e821281e2980e24f101774f6e1bfd8e4019aaa1
Frank-Proulx <frankproulx7@gmail.com>
2021-12-10 08:59:26 -0800

    add first test having received good fail

---
+++ lib/anagrams.rb
@@ -0,0 +1,6 @@
+class Anagrams
+
+  def checker(word1, word2)
+    
+  end
+end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,3 +1,9 @@
 require('rspec')
 require('anagrams')
 
+describe('Anagrams#checker') do
+  it('will take two words as arguements and check if they are anagrams, returning a phrase if they are') do
+    words = Anagrams.new()
+    expect(words.checker("listen", "silent")).to(eq("These words are anagrams."))
+  end
+end
