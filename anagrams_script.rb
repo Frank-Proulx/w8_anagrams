ff67e46a2a08eca07cefe4c98cc1c2f49ac4e18a
Frank-Proulx <frankproulx7@gmail.com>
2021-12-10 12:15:23 -0800

    add message indicating that numbers will be removed from entries

--- anagrams_script.rb
+++ anagrams_script.rb
@@ -6,5 +6,8 @@ puts "To check for anagrams or antigrams, input your first word or phrase"
 string1 = gets.chomp
 puts "Please enter your second word or phrase"
 string2 = gets.chomp
+if (string1.split('').any?{|x| x.match(/\d/)}) || (string2.split('').any?{|x| x.match(/\d/)})
+  puts "Your entry contains numbers, and words do not, so your type has been graciously removed for you"
+end
 checking = Anagrams.new(string1, string2)
 puts checking.checker
